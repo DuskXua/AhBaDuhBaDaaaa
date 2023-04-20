0326f123ce3793e84a48449275008cee8e672600
Anext0n <131202899+Anext0n@users.noreply.github.com>
2023-04-20 16:07:52 -0400

    Fixed island lvl added stat scrpt
    ^^

---
+++ Assets/Scripts/Gameplay/Stats.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    //Creates Stat Values in inspector -- Maybe theres a way we can use other scrips to effect this one? EX: Speed potion & leveling up
+    public float speed;
+    public float strength;
+    public float intelegence;
+    public float accuracy;
+    public float vitality;
+    public float hp;
+    public float ap;
+    public float sp;
+}
